23718034d7aa84bb05b4e01cb9a87c274d780106
Szilard Domotor <dom.szilard@gmail.com>
2017-10-20 22:18:48 +0200

    header, nav, home, post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'users/forgotten'
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20171020171830_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :name
+      t.integer :karma
+      t.text :about
+
+      t.timestamps
+    end
+  end
+end
