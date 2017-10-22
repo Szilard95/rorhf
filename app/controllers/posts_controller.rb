30b103827836775cc0bb314999efce644f173e84
Szilard Domotor <dom.szilard@gmail.com>
2017-10-22 02:21:38 +0200

    nicer comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,21 +14,13 @@ class PostsController < ApplicationController
                      score: rand(100), created_at: Time.now)
 
     comments = []
-    comments.push(Comment.new(id: 1, post: @post, user: @user, body: 'comment #1', score: rand(100),
-                              created_at: Time.now))
-    comments.push(Comment.new(id: 2, post: @post, user: @user, body: 'comment #2', score: rand(100),
-                              created_at: Time.now))
-    comments.push(Comment.new(id: 3, post: @post, user: @user, body: 'comment #3', score: rand(100),
-                              created_at: Time.now))
-    comments.push(Comment.new(id: 4, post: @post, user: @user, body: 'subcomment #4 for #1', score: rand(100),
-                              created_at: Time.now, parent_id: 1))
-    comments.push(Comment.new(id: 5, post: @post, user: @user, body: 'subcomment #5 for #4', score: rand(100),
-                              created_at: Time.now, parent_id: 4))
-    comments.push(Comment.new(id: 6, post: @post, user: @user, body: 'subcomment #6 for #3', score: rand(100),
-                              created_at: Time.now, parent_id: 3))
+    (1 + rand(5)).times do |i|
+      comments.push(Comment.new(id: i, post: @post, user: @user, body: "comment ##{i}", score: rand(100),
+                                created_at: Time.now))
+    end
     rand(15).times do |i|
-      parent = 1 + rand(comments.size)
-      comments.push(Comment.new(id: 6 + i + 1, post: @post, user: @user, body: "subcomment ##{6 + i + 1} for ##{parent}", score: rand(100),
+      parent = rand(comments.size)
+      comments.push(Comment.new(id: comments.size, post: @post, user: @user, body: "subcomment ##{comments.size} for ##{parent}", score: rand(100),
                                 created_at: Time.now, parent_id: parent))
     end
     @post.comments = comments
